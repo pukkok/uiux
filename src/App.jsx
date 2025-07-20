2ccc703982be98570c57ff684fa19ff929aee6ec
Pukkok <krystalgggg@gmail.com>
2025-07-20 16:59:19 +0900

    [Feat] 패키지 초기화

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+const App = () => {
+
+  return (
+    <div className="bg-[#111] flex w-full h-screen items-center justify-center">
+      <h1 className="text-gray-50 text-lg font-bold">VITE + TAILWIND</h1>
+    </div>
+  )
+}
+
+export default App
